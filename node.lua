c050ca3f7b29371e6c0a132e959aa25498cae8fd
Jim Siler <j.siler@jimsiler.com>
2022-01-21 15:42:45 -0500

    Try to add AM/PM field into config.html.  Debugging loop

--- node.lua
+++ node.lua
@@ -1069,7 +1069,7 @@ local function TimeTile(asset, config, x1, y1, x2, y2)
 		  localhout = localhour - 12
 	       end
 	    end
-	    log("TimeTile", "Line 1072, fmt=%s", fmt)
+	    log("TimeTile", "Line 1072, fmt=%s", "XYZZY")
 	    local time = string.format(fmt, localhour, min, sec)
 	    log("TimeTile", "Line 1074")
 	    if ampm then
