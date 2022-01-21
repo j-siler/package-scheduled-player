2ba7be056df324f675c342ae6f5b7797ed7bf3d8
Jim Siler <j.siler@jimsiler.com>
2022-01-21 14:00:33 -0500

    Try to add AM/PM field into config.html.  Debugging loop

--- node.lua
+++ node.lua
@@ -1062,12 +1062,12 @@ local function TimeTile(asset, config, x1, y1, x2, y2)
 	 log("TimeTile", "function(starts, ends), starts=%d, ends=%d", starts, ends)
 	 for now in helper.frame_between(starts, ends) do
 	    log("TimeTile", "In for loop (1)")
-	    log("TimeTile", "In for loop (2)") --, starts=%d, ends=%d, t=%d, prevtime=%d",
-		--starts
-		--,ends
+	    log("TimeTile", "In for loop (2) starts=%d, ends=%d,-- t=%d, prevtime=%d",
+		starts
+		,ends
 		--,t
 		--,prevtime
-	    --)
+	    )
 	    t = clock.since_midnight()
 	    if(t ~= prevtime)
 	    then
