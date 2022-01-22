8bf2062b8f0de1a3a830c124626e1da081f6cfef
Jim Siler <j.siler@jimsiler.com>
2022-01-22 17:59:27 -0500

    Try to add AM/PM field into config.html

--- node.lua
+++ node.lua
@@ -1029,7 +1029,8 @@ local function CountdownTile(asset, config, x1, y1, x2, y2)
 end
 
 local function TimeTile(asset, config, x1, y1, x2, y2)
-   print "TimeTile says Hello"
+   local dummyvar="TimeTile says Howdy!"
+   print dummyvar 
    log("TimeTile", "Entered function TimeTile")
    -- if node_config.timne_fmt ~= nil then
    --    log("TimeTile", "time_fmt=%s", node_config.time_fmt)
