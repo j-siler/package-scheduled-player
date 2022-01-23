cf28b452cff68c0fd9033522d91a190d50bce2b3
Jim Siler <j.siler@jimsiler.com>
2022-01-22 21:21:42 -0500

    Try to add AM/PM field into config.html

--- node.lua
+++ node.lua
@@ -1029,7 +1029,7 @@ local function CountdownTile(asset, config, x1, y1, x2, y2)
 end
 
 local function dump(o)
-   return tostring("TimeTile Testing: ")..tostring(0)
+   return tostring("TimeTile Testing: ")..tostring(o)
 --       if type(o) == 'table' then
 --          local s = '{ '
 --          for k,v in pairs(o) do
